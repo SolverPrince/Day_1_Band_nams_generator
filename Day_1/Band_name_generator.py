1e91d0f36af062cfa5c3c6e02431f5fb13f8c9cc
Prince Sareen <98515024+SolverPrince@users.noreply.github.com>
2026-02-25 12:25:06 +0530

    Add files via upload

---
+++ Day_1/Band_name_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the python band name generator".upper())
+city_name=input("Enter the name of the city where you born in\n".upper())
+pet_name=input("Enter the name of your per\n".upper())
+band_name_is=print(f"Your band name is: {city_name} {pet_name}")
